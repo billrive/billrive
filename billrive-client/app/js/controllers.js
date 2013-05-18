a306814eee887c972d848c691633f615bd49e69d
Prashanth Batchu <prashanth.batchu@billrive.com>
2013-05-18 13:32:34 -0600

    added jquery as it is apparently needed by bootstrap. Modified Controller structure and tested looping through sample bill json.

--- billrive-client/app/js/controllers.js
+++ billrive-client/app/js/controllers.js
@@ -1,11 +1,14 @@
-'use strict';
+//'use strict';
 
 /* Controllers */
-
+/*
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', [function($scope) {
+ $scope.todos = [
+    {text:'learn angular', done:true},
+    {text:'build an angular app', done:false}];
+ 
   }])
   .controller('MyCtrl2', [function() {
 
-  }]);
+  }]);*/
