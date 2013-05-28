f4dc13057d190bb2dd36fb0b90fd4fe7dab5b226
Prashanth Batchu <prashanth.batchu@billrive.com>
2013-05-28 07:29:51 -0600

    added underscore, moment, fastclick, angular-strap.

--- billrive-client/app/js/controllers.js
+++ billrive-client/app/js/controllers.js
@@ -1,5 +1,27 @@
 billRive.controller('billController', function($scope, billService) {
   billService.getBills(function(data) {
      $scope.bills = data;
+     $scope.username ="Prashanth";
+     $scope.payers= [
+{name:'John Doe'},
+{name:'Kristen Stewart'},
+{name:'Trevor Pott'},
+{name:'Hilda Hess'},
+{name:'Gavin Mays'}
+];
+     function addBill(){
+         
+//         $scope.bills.put();
+     };
+     
+      function deleteBill(){
+         
+         
+     };
+     
+      function editBill(){
+         
+         
+     };
   });
 });
