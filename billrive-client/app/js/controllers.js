827f298d697a9ce0c1fcc7f6070767618e7012ad
Prashanth Batchu <prashanth.batchu@billrive.com>
2013-06-08 10:36:54 -0600

    converted service to a factory. Moved all JSON manual setting to factory. More structure.

--- billrive-client/app/js/controllers.js
+++ billrive-client/app/js/controllers.js
@@ -1,46 +1,45 @@
 billRive.controller('billController', function($scope, billService) {
-  billService.getBills(function(data) {
-     $scope.bills = data;
-     $scope.username ="Prashanth";
-     $scope.payers= [
-{name:'John Doe'},
-{name:'Kristen Stewart'},
-{name:'Trevor Pott'},
-{name:'Hilda Hess'},
-{name:'Gavin Mays'}
-];
-
-
-$scope.friends= [
-{name:'John Doe'},
-{name:'Kristen Stewart'},
-{name:'Trevor Pott'},
-{name:'Hilda Hess'},
-{name:'Gavin Mays'}
-];
-
-
-$scope.groups= [
-{name:'SLC Roommates'},
-{name:'Denver Old Buddies'},
-{name:'Ski Club'},
-{name:'Family'},
-{name:'India'}
-];
-
-     function addBill(){
-         
-//         $scope.bills.put();
-     };
-     
-      function deleteBill(){
-         
-         
-     };
-     
-      function editBill(){
-         
-         
-     };
-  });
+  
+  $scope.bills= billService.getBills();
+  
+  $scope.friends=billService.getFriends();
+  $scope.groups=billService.getGroups();
+  $scope.payers=billService.getPayers();
+//    billService.getBills(function(data) {
+//     $scope.bills = data;
+//     $scope.username ="Prashanth";
+//    
+//
+//$scope.friends= [
+//{name:'John Doe'},
+//{name:'Kristen Stewart'},
+//{name:'Trevor Pott'},
+//{name:'Hilda Hess'},
+//{name:'Gavin Mays'}
+//];
+//
+//
+//$scope.groups= [
+//{name:'SLC Roommates'},
+//{name:'Denver Old Buddies'},
+//{name:'Ski Club'},
+//{name:'Family'},
+//{name:'India'}
+//];
+//
+//     function addBill(){
+//         
+////         $scope.bills.put();
+//     };
+//     
+//      function deleteBill(){
+//         
+//         
+//     };
+//     
+//      function editBill(){
+//         
+//         
+//     };
+//  });
 });
