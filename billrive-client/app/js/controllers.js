6c7222d90db74da4a7a0783fc6f83fb0c2d636b4
Prashanth Batchu <prashanth.batchu@billrive.com>
2013-06-09 12:51:33 -0600

    added ability to sum up calculated total.

--- billrive-client/app/js/controllers.js
+++ billrive-client/app/js/controllers.js
@@ -1,7 +1,4 @@
 billRive.controller('billController', function($scope, billService) {
-
-    //$scope.bills= billService.getBills();
-
     $scope.friends = billService.getFriends();
     $scope.groups = billService.getGroups();
     $scope.payers = billService.getPayers();
@@ -12,10 +9,7 @@ billRive.controller('billController', function($scope, billService) {
     $scope.addBill = function() {
 
     };
-    //console.log( $scope.friends);
-
     $scope.setBillGroup = function() {
-//         console.log(id);
         var $groupId = $scope.bill.groupId;
 
         var $groupMembers;
@@ -27,68 +21,29 @@ billRive.controller('billController', function($scope, billService) {
             }
         }
         var $groupUserAndLiableCost = [];
-        console.log($scope.friends[0].id);
         var $friendNamefromId = null;
         for (i = 0; i < $groupMembers.length; i++) {
 
             for (var j = 0; j < $scope.friends.length; j++) {
-
-                //
                 if ($scope.friends[j].id == $groupMembers[i])
-                {
                     $friendNamefromId = $scope.friends[j].name;
-                }
-                //
             }
             $groupUserAndLiableCost.push({userId: $groupMembers[i], liableCost: null, name: $friendNamefromId});
         }
-
-        //console.log($groupUserAndLiableCost);
-//         for(var i=0;i<)
-//         var $userAndLiableCost=
-        $scope.bill.billFinances.billItemEntry = $groupUserAndLiableCost;
+        $scope.bill.billFinances.billSimpleTotals.userIdAndLiableCost = $groupUserAndLiableCost;
         $groupUserAndLiableCost = [];
-        //$scope.bill.billFinances.billItemEntry.push($groupUserAndLiableCost);
-        //console.log($userAndLiableCost);
+
     };
 
+$scope.calculateSimpleSum=function(){
 
-//    billService.getBills(function(data) {
-//     $scope.bills = data;
-//     $scope.username ="Prashanth";
-//    
-//
-//$scope.friends= [
-//{name:'John Doe'},
-//{name:'Kristen Stewart'},
-//{name:'Trevor Pott'},
-//{name:'Hilda Hess'},
-//{name:'Gavin Mays'}
-//];
-//
-//
-//$scope.groups= [
-//{name:'SLC Roommates'},
-//{name:'Denver Old Buddies'},
-//{name:'Ski Club'},
-//{name:'Family'},
-//{name:'India'}
-//];
-//
-//     function addBill(){
-//         
-////         $scope.bills.put();
-//     };
-//     
-//      function deleteBill(){
-//         
-//         
-//     };
-//     
-//      function editBill(){
-//         
-//         
-//     };
-//  });
+ $scope.simpleCalculatedTotal=0;
+ for (i = 0; i < $scope.bill.billFinances.billSimpleTotals.userIdAndLiableCost.length; i++) {
+     $scope.simpleCalculatedTotal+= parseInt($scope.bill.billFinances.billSimpleTotals.userIdAndLiableCost[i].liableCost);
+ }
+
+    
+};
+
 });
 
