76c5f2cd09c9c0bea0def53c3c871ae220a10ef8
Prashanth Batchu <prashanth.batchu@billrive.com>
2013-09-15 13:09:45 -0600

    Restructuring Client application in progress.

--- billrive-client/app/js/ctrl/userCtrl.js
+++ billrive-client/app/js/ctrl/userCtrl.js
@@ -1,7 +1,7 @@
 billRive.controller('userCtrl', function( userService,$scope) {
   // Call the async method and then do stuff with what is returned inside our own then function
   userService.async().then(function(d) {
-    $scope.user = d;
+    $scope.user = d.data;
   });
 });
 
