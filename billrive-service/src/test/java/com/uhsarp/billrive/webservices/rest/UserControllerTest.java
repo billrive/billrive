c43b44ca34d6eda3ac3c1583b2315759cf81d598
Uhsarp <Uhsarp@trisha>
2014-01-19 22:25:51 -0700

    Began to write up the test cases for Controllers. Commented out the test code to figure out an outstanding issue.

---
+++ billrive-service/src/test/java/com/uhsarp/billrive/webservices/rest/UserControllerTest.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.uhsarp.billrive.webservices.rest;
+
+
+import com.uhsarp.billrive.domain.User;
+import com.uhsarp.billrive.services.UserService;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.request.WebRequest;
+
+/**
+ *
+ * @author Uhsarp
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("classpath:billrive-context-test.xml")
+public class UserControllerTest {
+    
+    public UserControllerTest() {
+    }
+    
+   private MockMvc mockMvc;
+   
+   @Autowired
+   UserController userController;
+    
+    @Autowired
+  private WebApplicationContext wac;
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+          this.mockMvc  = webAppContextSetup(this.wac).build();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getUser method, of class UserController.
+     */
+    @Test
+    public void testGetUser() {
+        System.out.println("getUser");
+        Long userId = 6L;
+        User expResult = null;
+        User result = userController.getUser(userId);
+        assertEquals("Bruce", result.getfName());
+        
+//         this.mockMvc.perform(get("/foo").accept("application/json"))
+//        .andExpect(status().isOk())
+//        .andExpect(content().mimeType("application/json"));
+
+
+    }
+
+    /**
+     * Test of addUser method, of class UserController.
+     */
+//    @Test
+//    public void testAddUser() {
+//        System.out.println("addUser");
+//        User user_p = null;
+//        HttpServletResponse httpResponse_p = null;
+//        WebRequest request_p = null;
+//        UserController instance = new UserController();
+//        User expResult = null;
+//        User result = instance.addUser(user_p, httpResponse_p, request_p);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getUserService method, of class UserController.
+//     */
+//    @Test
+//    public void testGetUserService() {
+//        System.out.println("getUserService");
+//        UserController instance = new UserController();
+//        UserService expResult = null;
+//        UserService result = instance.getUserService();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of setUserService method, of class UserController.
+//     */
+//    @Test
+//    public void testSetUserService() {
+//        System.out.println("setUserService");
+//        UserService userService = null;
+//        UserController instance = new UserController();
+//        instance.setUserService(userService);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+}
