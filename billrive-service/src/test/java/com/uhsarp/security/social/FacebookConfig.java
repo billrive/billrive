39222d37bdb67c7cb3c591473fd1e519e292b87c
Prashanth Batchu <prashanth@batchu.net>
2014-02-04 14:24:46 -0700

    commented out imports/content of FacebookConfig to avoid build problems (as deps are disabled in pom).

--- billrive-service/src/test/java/com/uhsarp/security/social/FacebookConfig.java
+++ billrive-service/src/test/java/com/uhsarp/security/social/FacebookConfig.java
@@ -6,37 +6,37 @@
 
 package com.uhsarp.security.social;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.social.UserIdSource;
-import org.springframework.social.config.annotation.EnableInMemoryConnectionRepository;
-import org.springframework.social.connect.ConnectionFactoryLocator;
-import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.connect.web.ConnectController;
-import org.springframework.social.facebook.config.annotation.EnableFacebook;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.social.UserIdSource;
+//import org.springframework.social.config.annotation.EnableInMemoryConnectionRepository;
+//import org.springframework.social.connect.ConnectionFactoryLocator;
+//import org.springframework.social.connect.ConnectionRepository;
+//import org.springframework.social.connect.web.ConnectController;
+//import org.springframework.social.facebook.config.annotation.EnableFacebook;
 
 /**
  *
  * @author Uhsarp
  */
-@EnableFacebook(appId="1416423315266898", appSecret="316aa8fe81e4bb33d500695a975c5167")
-@EnableInMemoryConnectionRepository //Replace this with @EnableJdbcConnectionRepository  for persistence
+//@EnableFacebook(appId="1416423315266898", appSecret="316aa8fe81e4bb33d500695a975c5167")
+//@EnableInMemoryConnectionRepository //Replace this with @EnableJdbcConnectionRepository  for persistence
 public class FacebookConfig {
 
 //     OAuth's Resource Owner Authorization is performed by ConnectController
 //    @EnableFacebook makes sure that the constructor args are automatically created and passed to connectController
-    @Bean
-    public ConnectController connectController(ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
-        return new ConnectController(connectionFactoryLocator, connectionRepository);
-    }
-
-    @Bean
-    public UserIdSource userIdSource() {
-        return new UserIdSource() {
-            @Override
-            public String getUserId() {
-                return "testuser";
-            }
-        };
-    }
+//    @Bean
+//    public ConnectController connectController(ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
+//        return new ConnectController(connectionFactoryLocator, connectionRepository);
+//    }
+//
+//    @Bean
+//    public UserIdSource userIdSource() {
+//        return new UserIdSource() {
+//            @Override
+//            public String getUserId() {
+//                return "testuser";
+//            }
+//        };
+//    }
 
 }
