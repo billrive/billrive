810290529016798696f99fabab7984442020cf23
Prashanth Batchu <prashanth@batchu.net>
2014-03-30 13:49:37 -0600

    updated Table List design. Added sketches of data model

--- billrive-client/app/js/directives.js
+++ billrive-client/app/js/directives.js
@@ -31,3 +31,27 @@ angular.module('billRive.directives', []).
         }
     };
 });
+
+
+billRive.directive('showBillDetails', function() {
+      return {
+        restrict: 'A',
+        require : 'ngModel',
+        link: function(scope, element, attrs, ngModelCtrl) {
+          element.datetimepicker({
+            dateFormat:'dd/MM/yyyy hh:mm:ss',
+            language: 'en-US'
+          }).on('changeDate', function(e) {
+
+            var outputDate = new Date(e.date);
+
+           var n = outputDate.getTime();
+
+
+           ngModelCtrl.$setViewValue(n);
+            scope.$apply();
+          });
+        }
+    };
+    
+});
